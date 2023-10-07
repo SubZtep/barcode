8893fd7c0d9331e8afad251c67b2bc1c8ba6425f
Andras Serfozo <subztep@gmail.com>
2023-10-07 13:11:48 +0100

    Add barcode detector

---
+++ src/components/CameraStream.tsx
@@ -0,0 +1,89 @@
+import { css } from "@emotion/css"
+import { createEffect, type ParentComponent, onMount, onCleanup, createSignal } from "solid-js"
+
+const CameraStream: ParentComponent<{
+  enabled: boolean
+  onStart?: (video: HTMLVideoElement) => void
+  onStop?: () => void
+}> = props => {
+  let video: HTMLVideoElement | undefined
+  let mediaStream: MediaStream | null = null
+  let barcodeDetector: any = null
+  let interval: number
+  const [result, setResult] = createSignal("Hello")
+
+  const videoLoaded = () => {
+    document.body.style.setProperty("--input-aspect-ratio", String(video!.videoWidth / video!.videoHeight))
+    // setState({ input: { width: video!.clientWidth, height: video!.clientHeight } })
+  }
+
+  onMount(() => {
+    video!.addEventListener("loadedmetadata", videoLoaded)
+
+    // @ts-ignore
+    const barcodeDetector = new BarcodeDetector({
+      formats: ["code_39", "code_128", "codabar", "ean_13"],
+    });
+  })
+
+  onCleanup(() => {
+    video!.removeEventListener("loadedmetadata", videoLoaded)
+  })
+
+  createEffect(async () => {
+    if (props.enabled) {
+      mediaStream = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: { ideal: "environment" } },
+        audio: false,
+      })
+      video!.srcObject = mediaStream
+
+      props.onStart?.(video!)
+
+      interval = setInterval(async () => {
+        const barcodes = await barcodeDetector.detect(video);
+        if (barcodes.length <= 0) return;
+        setResult(barcodes.map(barcode => barcode.rawValue));
+      }, 1000)
+
+    } else {
+      clearInterval(interval)
+      props.onStop?.()
+      video!.srcObject = null
+      mediaStream?.getTracks().forEach(track => {
+        if (track.readyState === "live") {
+          track.stop()
+        }
+      })
+      mediaStream = null
+    }
+  })
+
+  onCleanup(() => {
+    if (props.enabled) {
+      props.onStop?.()
+    }
+  })
+
+  return (
+    <>
+    <video
+      ref={video}
+      playsinline
+      autoplay
+      muted
+      // @ts-ignore
+      disablePictureInPicture={true}
+      class={css`
+        height: 480px;
+        max-width: 100%;
+        background: red;
+      `}
+    ></video>
+    <br />
+    {result()}
+    </>
+  )
+}
+
+export default CameraStream
--- src/views/Scan.tsx
+++ src/views/Scan.tsx
@@ -1,5 +1,6 @@
 import { Show, createSignal, For, onMount } from "solid-js"
 import { css } from "@emotion/css"
+import CameraStream from "../components/CameraStream"
 
 function Scan() {
   const [formats, setFormats] = createSignal<string[]>([])
@@ -7,18 +8,6 @@ function Scan() {
   onMount(async () => {
     // @ts-ignore
     setFormats(await BarcodeDetector.getSupportedFormats())
-
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: {
-        facingMode: { ideal: "environment" },
-      },
-      audio: false,
-    })
-
-    const videoEl = document.querySelector("#stream") as HTMLVideoElement
-    videoEl.srcObject = stream
-
-    await videoEl.play()
   })
 
   return (
@@ -28,12 +17,15 @@ function Scan() {
       `}
     >
       <Show when={Array.isArray(formats())}>
-        <select>
-          <For each={formats()}>{format => <option>{format}</option>}</For>
-        </select>
+        <label class={css`display: block; margin: 1rem;`}>
+          Barcode format:&nbsp;
+          <select>
+            <For each={formats()}>{format => <option>{format}</option>}</For>
+          </select>
+        </label>
       </Show>
 
-      <video id="stream" style="width: 100vw; height: 100vh;" />
+      <CameraStream enabled={true} />
     </div>
   )
 }
